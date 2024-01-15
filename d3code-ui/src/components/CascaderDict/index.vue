405c1f5a0d396ab200eb4e8fb46546bd84797f9f
angle <3210898823@qq.com>
2024-01-15 17:06:06 +0800

    支持多级字典标签

---
+++ d3code-ui/src/components/CascaderDict/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="cascader">
+        <el-cascader ref="refCascader" :value="value" :options="optData" @change="changeDict"></el-cascader>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cascader',
+    props: {
+        optData: Array
+    },
+    data() {
+        return {
+            value: []
+        }
+    },
+    created(){
+        
+    },
+    methods: {
+        changeDict(e) {
+            this.$emit('selectDict', e.join('$$$'))
+        }
+    }
+}
+</script>
--- d3code-ui/src/components/DictTag/index.vue
+++ d3code-ui/src/components/DictTag/index.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-    <template v-for="(item, index) in options">
-      <template v-if="values.includes(item.value)">
-        <span
-          v-if="item.raw.listClass == 'default' || item.raw.listClass == ''"
-          :key="item.value"
-          :index="index"
-          :class="item.raw.cssClass"
-          >{{ item.label }}</span
-        >
-        <el-tag
-          v-else
-          :disable-transitions="true"
-          :key="item.value"
-          :index="index"
-          :type="item.raw.listClass == 'primary' ? '' : item.raw.listClass"
-          :class="item.raw.cssClass"
-        >
-          {{ item.label }}
-        </el-tag>
+    <!-- 一级字典 -->
+    <template v-if="!isCascader">
+      <template v-for="(item, index) in options">
+        <template v-if="values.includes(item.value)">
+          <span v-if="item.raw.listClass == 'default' || item.raw.listClass == ''" :key="item.value" :index="index"
+            :class="item.raw.cssClass">{{ item.label }}</span>
+          <el-tag v-else :disable-transitions="true" :key="item.value" :index="index"
+            :type="item.raw.listClass == 'primary' ? '' : item.raw.listClass" :class="item.raw.cssClass">
+            {{ item.label }}
+          </el-tag>
+        </template>
       </template>
     </template>
+    <!-- 多级字典 -->
+    <template v-else v-for="(item, index) in cascaderOptions">
+      <span v-if="item.raw.listClass == 'default' || item.raw.listClass == ''" :key="item.value" :index="index"
+        :class="item.raw.cssClass">{{ item.label }}</span>
+      <el-tag v-else :disable-transitions="true" :key="item.value" :index="index"
+        :type="item.raw.listClass == 'primary' ? '' : item.raw.listClass" :class="item.raw.cssClass">
+        {{ item.label }}
+      </el-tag>
+    </template>
+
   </div>
 </template>
 
@@ -35,6 +37,17 @@ export default {
     value: [Number, String, Array],
   },
   computed: {
+    isCascader() {
+      switch (this.value.indexOf('$$$')) {
+        case -1:
+          return false
+        default:
+          return true
+      }
+    },
+    cascaderValue() {
+      return this.value.split('$$$')
+    },
     values() {
       if (this.value !== null && typeof this.value !== 'undefined') {
         return Array.isArray(this.value) ? this.value : [String(this.value)];
@@ -42,7 +55,22 @@ export default {
         return [];
       }
     },
-  },
+    cascaderOptions() {
+      var arr = []
+      var op = this.options
+      for (var index = 0; index < this.cascaderValue.length; index++) {
+        for (var i = 0; i < op.length; i++) {
+          if (this.cascaderValue[index] === op[i].value) {
+            arr.push(op[i])
+            op = op[i].children
+            if (op === null) {
+              return arr
+            }
+          }
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
